877565978c2000ed24a7a05009ad9fad1c541f22
zach-martin <zach.martin@lifefitness.com>
2017-12-26 12:45:02 -0600

    Completed SQLAlchemy integration. Fixed broken package names.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Missive Notepad Web App
+# Missive Notepad
 
 Note taking web app based on the Flask tutorial found [here](http://flask.pocoo.org/docs/0.12/tutorial/ "Flask Tutorial"). This is just a little app created to help familiarize myself with Flask and SQLalchemy. It is not really intended to be used in any way in its current state.
--- missive/blueprints/routes.py
+++ missive/blueprints/routes.py
@@ -1,5 +1,5 @@
 import os
-from missive.models import db
+from missive.models import db, Entries
 from flask import Blueprint, request, session, g, redirect, url_for, abort, \
      render_template, flash, current_app
 
@@ -8,42 +8,34 @@ bp = Blueprint('missive', __name__)
 
 @bp.route('/')
 def show_entries():
-    pass
-    # db = get_db()
-    # cur = db.execute('select title, text from entries order by id desc')
-    # entries = cur.fetchall()
-    # return render_template('show_entries.html', entries=entries)
+    entries = Entries.query.all()
+    return render_template('show_entries.html', entries=entries)
 
 @bp.route('/add', methods=['POST'])
 def add_entry():
-    pass
-    # if not session.get('logged_in'):
-    #     abort(401)
-    # db = get_db()
-    # db.execute('insert into entries (title, text) values (?, ?)',
-    #              [request.form['title'], request.form['text']])
-    # db.commit()
-    # flash('New entry was successfully posted')
-    # return redirect(url_for('show_entries'))
+    if not session.get('logged_in'):
+        abort(401)
+    db.session.add(Entries(title=request.form['title'], text=request.form['text']))
+    db.session.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('missive.show_entries'))
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
-    # error = None
-    # if request.method == 'POST':
-    #     if request.form['username'] != current_app.config['USERNAME']:
-    #         error = 'Invalid username'
-    #     elif request.form['password'] != current_app.config['PASSWORD']:
-    #         error = 'Invalid password'
-    #     else:
-    #         session['logged_in'] = True
-    #         flash('You were logged in')
-    #         return redirect(url_for('show_entries'))
-    # return render_template('login.html', error=error)
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != current_app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != current_app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('missive.show_entries'))
+    return render_template('login.html', error=error)
 
 @bp.route('/logout')
 def logout():
-    pass
-    # session.pop('logged_in', None)
-    # flash('You were logged out')
-    # return redirect(url_for('show_entries'))
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('missive.show_entries'))
